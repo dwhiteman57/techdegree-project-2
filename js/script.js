e3a6294dbf0e36855fa441ae0c626e43b9a4ebb1
Daniel Whiteman <daniel.whiteman1@gmail.com>
2018-11-13 09:57:33 -0600

    Unit 2 Project
    Confirmed showPage function is now working.

--- js/script.js
+++ js/script.js
@@ -9,12 +9,6 @@ FSJS project 2 - List Filter and Pagination
 /***
    Add your global variables that store the DOM elements you will
    need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
 ***/
 const studentList = document.querySelector(".student-list");
 const studentItems = document.querySelectorAll(".student-item");
